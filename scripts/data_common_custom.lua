ca413a502bbc376c6bdb04ab8371150d73fabd0f
Dogfisc <92417873+Dogfisc@users.noreply.github.com>
2021-10-12 21:12:35 -0700

    Create data_common_custom.lua

---
+++ scripts/data_common_custom.lua
@@ -0,0 +1,34 @@
+-- 
+-- Please see the license.txt file included with this distribution for 
+-- attribution and copyright information.
+--
+
+-- NPC range properties
+-- [weapon name] = {range increment, maximumm range increments}
+weaponranges = {
+	["blowgun"] = {20, 10},
+	["bolas"] = {10, 5},
+	["club"] = {10, 5},
+	["composite longbow"] = {110, 10},
+	["composite shortbow"] = {70, 10},
+	["dagger"] = {10, 5},
+	["dart"] = {20, 5},
+	["halfling sling staff"] = {80, 10},
+	["hand crossbow"] = {30, 10},
+	["heavy crossbow"] = {120, 10},
+	["javelin"] = {30, 5},
+	["light crossbow"] = {80, 10},
+	["light hammer"] = {20, 5},
+	["longbow"] = {100, 10},
+	["net"] = {10, 5},
+	["reapeating heavy crossbow"] = {120, 10},
+	["repeating light crossbow"] = {80, 10},
+	["shortbow"] = {30, 10},
+	["shortspear"] = {20, 5},
+	["shuriken"] = {10, 5},
+	["sling"] = {50, 10},
+	["spear"] = {20, 5},
+	["starknife"] = {20, 5},
+	["throwinig axe"] = {10, 5},
+	["trident"] = {10, 5}
+};
